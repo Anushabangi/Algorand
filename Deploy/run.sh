505837b5529bea222a14ca396697b12a36b1e945
Zhiyi Xu <yzxu@ucdavis.edu>
2019-02-19 00:14:12 -0800

    script dev

--- Deploy/run.sh
+++ Deploy/run.sh
@@ -2,20 +2,37 @@
 
 osascript -e 'tell app "Terminal"
     do script "cd Downloads/expodb-bc-docker;
+    docker stop dev;
+    docker start dev;
+    docker exec -it dev bash;
+    cd expo;
+    ./rundb -nid0"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd Downloads/expodb-bc-docker;
+    docker stop s1;
+    docker start s1;
     docker exec -it s1 ./rundb -nid1"
 end tell'
 
 osascript -e 'tell app "Terminal"
     do script "cd Downloads/expodb-bc-docker;
+    docker stop s2;
+    docker start s2;
     docker exec -it s2 ./rundb -nid2"
 end tell'
 
 osascript -e 'tell app "Terminal"
     do script "cd Downloads/expodb-bc-docker;
+    docker stop s3;
+    docker start s3;
     docker exec -it s3 ./rundb -nid3"
 end tell'
 
 osascript -e 'tell app "Terminal"
     do script "cd Downloads/expodb-bc-docker;
+    docker stop c4;
+    docker start c4;
     docker exec -it c4 ./runcl -nid4"
 end tell'
