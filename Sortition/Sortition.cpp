21ec64b71323447527c810200eff95499b24e1df
Hanqi <hqdu@ucdavis.edu>
2019-02-24 22:34:26 -0800

    add sortition cpp

--- Sortition/Sortition.cpp
+++ Sortition/Sortition.cpp
@@ -1,1 +1,11 @@
 #include <string>
+sortition(sk, seed, threshold, role, w, W) {
+	<hash, proof> = VRF(sk, seed, role);
+	p = threshold / W;
+	j = 0;
+	while(hash / 2^hashlen < sum(0, j, B(k,w,p)) || hash / 2^hashlen >= sum(0, j+1, B(k, w, p))) {
+		j++;
+	}
+	return <hash, proof, j>;
+}
+
