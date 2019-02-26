63c3cf9c87b15bd19330c95ddeef40981d1c75c6
ltzx <yzxu@ucdavis.edu>
2019-02-25 20:50:15 -0800

    linex

--- Deploy/runL.sh
+++ Deploy/runL.sh
@@ -1,28 +1,47 @@
 #!/bin/sh
-DIR="Downloads/expodb-bc-docker"
-xterm -hold -e "cd $DIR;
-    docker stop dev;
-    docker start dev;
-    sudo docker exec -it dev bash;
-    cd expo;
-    ./rundb -nid0"
+DIR="~/Desktop/expodb-bc-docker"
 
-xterm -hold -e "cd $DIR;
-    docker stop s1;
-    docker start s1;
-    docker exec -it s1 ./rundb -nid1"
+docker stop dev;
+docker stop s1;
+docker stop s2;
+docker stop s3;
+docker stop c4;
+docker start dev;
+docker start s1;
+docker start s2;
+docker start s3;
+docker start c4;
 
-xterm -hold -e "cd $DIR;
-    docker stop s2;
-    docker start s2;
-    docker exec -it s2 ./rundb -nid2"
+devIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dev)
+s1IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' s1)
+s2IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' s2)
+s3IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' s3)
+c4IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' c4)
+echo "$devIP\n$s1IP\n$s2IP\n$s3IP\n$c4IP" > "ExpoDB-BC/ifconfig.txt"
 
-xterm -hold -e "cd $DIR;
-    docker stop s3;
-    docker start s3;
-    docker exec -it s3 ./rundb -nid3"
+gnome-terminal -- bash -c "echo dev;
+  cd $DIR;
+  docker exec -it dev bash;
+  cd expo;
+  ./rundb -nid0;
+  exec bash"
 
-xterm -hold -e "cd $DIR;
-    docker stop c4;
-    docker start c4;
-    docker exec -it c4 ./runcl -nid4"
+gnome-terminal -- bash -c "echo s1;
+  cd $DIR;
+  docker exec -it s1 ./rundb -nid1 > logs1.txt;
+  exec bash"
+
+gnome-terminal -- bash -c "echo s2;
+  cd $DIR;
+  docker exec -it s2 ./rundb -nid2 > logs2.txt;
+  exec bash"
+
+gnome-terminal -- bash -c "echo s3;
+  cd $DIR;
+  docker exec -it s3 ./rundb -nid3 > logs3.txt;
+  exec bash"
+
+gnome-terminal -- bash -c "echo c4;
+  cd $DIR;
+  docker exec -it c4 ./runcl -nid4 > logc4.txt;
+  exec bash"
